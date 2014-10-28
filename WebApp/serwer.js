cd9a1c4c2fb2f70b927c7a407397996b2d8f797d
Maciej Kucharski <maciekek@gmail.com>
2014-10-28 11:57:40 +0100

    Update serwer.js
    changes for travis

--- WebApp/serwer.js
+++ WebApp/serwer.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 
-var port = 80;
+var port = 3000;
 
 app.use(express.static(__dirname + '/app'));
 app.use(bodyParser.urlencoded({
